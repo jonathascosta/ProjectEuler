b5481a04596d7ffcc3257f2d1b2f03d73be17a3f
Jonathas Costa <jonathas.costa@gmail.com>
2011-11-09 01:49:12 -0200

    Refactoring

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace ProjectEuler
 {
@@ -9,7 +10,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Solução = {0}", Problem014.Solve());
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            var solution = Problem012.Solve();
+
+            watch.Stop();
+            Console.WriteLine("Solução = {0}", solution);
+            Console.WriteLine("Execution time: {0} milliseconds", watch.ElapsedMilliseconds);
             Console.ReadLine();
         }
     }
